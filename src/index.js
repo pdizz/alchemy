052d8b85eaee5a2ca2a6c82c581bb82be519188d
pdizz <pete.albrecht@gmail.com>
2016-12-21 19:50:43 -0700

    ingredient list items that share matching effects with selected items are highlighted

--- src/index.js
+++ src/index.js
@@ -18,20 +18,32 @@ new Vue({
         selectedEffects: []
     },
     computed: {
-        filteredIngredients: function() {
+        filteredIngredients: function () {
             var query = String(this.ingredientsQuery).toLowerCase();
             return this.ingredients.filter(function (ingredient) {
                 return String(ingredient.name).toLowerCase().indexOf(query) > -1;
             });
         },
-        filteredEffects: function() {
+        filteredEffects: function () {
             var query = String(this.effectsQuery).toLowerCase();
             return this.effects.filter(function (effect) {
                 return String(effect.name).toLowerCase().indexOf(query) > -1;
             });
         },
-        foundRecipes: function() {
+        foundRecipes: function () {
             return recipe.findRecipes(this.selectedIngredients);
+        }
+    },
+    methods: {
+        // Does the ingredient list item have matching effects with any of the selected items?
+        matchesSelected: function (ingredient) {
+            return this.selectedIngredients.reduce(function (isMatch, selectedIngredient) {
+                var match = recipe.matchIngredients([selectedIngredient, ingredient]);
+                if (match.length > 0) {
+                    isMatch = true;
+                }
+                return isMatch;
+            }, false);
         }
     }
 });
